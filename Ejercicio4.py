67a8398149d17ac3cef6f1f115052ccf949039d5
Maria Parra <maria.parra05@usa.edu.co>
2025-03-13 11:35:11 -0500

    Actualizar Ejercicio4.py

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,1 +1,15 @@
+import struct
 
+# Convertir un número decimal a punto flotante (IEEE 754)
+
+def decimal_a_punto_flotante(num):
+
+return format(struct.unpack('!I, struct.pack('f', num))[0], '032b')
+
+#Ejemplo de uso
+
+numero_decimal = -10.5
+
+punto_flotante decimal_a_punto_flotante(numero_decimal)
+
+print('El número (numero_decimal) en punto flotante es: (punto_flotante}")
